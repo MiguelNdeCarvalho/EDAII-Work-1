d4cb646ed15742bd28625d03af644cf998596dfc
filipealfaiate <filalfa1.fa@gmail.com>
2021-04-14 00:45:36 +0100

    add new files

---
+++ Solution.java
@@ -0,0 +1,13 @@
+public class Solution
+{
+	int maxPeixes;
+	int dist;
+	int rt;
+
+	public void setSolution(int maxPeixes, int dist, int rt)
+	{
+		this.maxPeixes = maxPeixes;
+		this.dist = dist;
+		this.rt = rt;
+	}
+}
