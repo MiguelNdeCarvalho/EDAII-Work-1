e0852148db2f122870bdef5d8c7e98edf606db9c
filipealfaiate <filalfa1.fa@gmail.com>
2021-04-19 15:14:27 +0100

    kill me now

--- CodFishing.java
+++ CodFishing.java
@@ -80,57 +80,37 @@ public class CodFishing
 		//associa um barco a cada ciclo ao local com mais peixes
 		for (int i = barcos.length - 1; i >= 0; i--)
 		{
+			//if (i > 0 && barcos[i].atributo == barcos[i - 1].atributo && Math.abs(barcos[i].x - peixes[peixes.length - 1].x) + Math.abs(barcos[i].y - peixes[peixes.length - 1].y) > Math.abs(barcos[i - 1].x - peixes[peixes.length - 1].x) + Math.abs(barcos[i - 1].y - peixes[peixes.length - 1].y))
+
 			//instacia de classe para casa associacao
 			Solution s = new Solution();
-			Solution s1 = new Solution();
 
-			//verifica se o a associacao ja se encontra calcula (isto e, com o melhor caso)
-			//caso nao estaja entra no if, caso esteja entra no else para copiar os valores do melhor caso
+			//verifica se o a associacao do barco i com o sitio do peixe ja se encontra calcula (isto e, com o melhor caso), caso nao estaja entra no if,
 			if (matriz_sol[i][peixes.length - 1] == null)
-				s.copy(pesquisa(matriz_sol, barcos, peixes, i, peixes.length - 1));
+			{
+				//se houve mais barcos que peixes por decidir e se a distancia do barco i em relacao ao sitio de peixes for maior que a distancia do barco i-1 em relacao ao mesmo sitio de peixes
+				//entao vai entrar neste if decrementando o barco
+				if (i > peixes.length - 1 && Math.abs(barcos[i].x - peixes[peixes.length - 1].x) + Math.abs(barcos[i].y - peixes[peixes.length - 1].y) > Math.abs(barcos[i - 1].x - peixes[peixes.length - 1].x) + Math.abs(barcos[i - 1].y - peixes[peixes.length - 1].y))
+					i--;
+			
+				s.copy(pesquisa(matriz_sol, barcos, peixes, i, peixes.length - 2));
 
+				//s.inc(barcos[i], peixes[peixes.length - 1]);
+			}
+
+			//caso esteja entra no else para copiar os valores do melhor caso
 			else
 				s.copy(matriz_sol[i][peixes.length - 1]);
 
-			System.out.println("ARRAY");
-				for (int j = 0; j < barcos.length; j++)
-					System.out.println(barcos[j].x);
+			//System.out.println("terminou a pesquisa da main do barco " + i + " com o S: " + s.maxPeixes + " " + s.dist + " " + s.rating);
 
-			if (i > 0 && barcos[i].atributo == barcos[i - 1].atributo)
-			{
-				System.out.println("fora do pesquisa barco " + i + " com rating " + barcos[i].atributo);
-				System.out.println("fora do pesquisa barco " + (i -1) + " com rating " + barcos[i -1].atributo);
-				Item aux = barcos[i];
-				barcos[i] = barcos[i - 1];
-				barcos[i - 1] = aux;
-
-				System.out.println("mostrar troca antes");
-				for (int j = 0; j < barcos.length; j++)
-					System.out.println(barcos[j].x);
-
-				s1.copy(pesquisa(matriz_sol, barcos, peixes, i, peixes.length -1));
-
-				aux = barcos[i];
-				barcos[i] = barcos[i - 1];
-				barcos[i - 1] = aux;
-
-				System.out.println("mostrar troca depois");
-				for (int j = 0; j < barcos.length; j++)
-					System.out.println(barcos[j].x);
-
-				if (s.maxPeixes <= s1.maxPeixes && s.dist >= s1.dist && s.rating >= s1.rating)
-					s.copy(s1);
-			}
-
-			System.out.println("terminou a pesquisa da main do barco " + i + " com o S: " + s.maxPeixes + " " + s.dist + " " + s.rating);
-
-			System.out.println("antes do if temp: " + temp.maxPeixes + " " + temp.dist + " " + temp.rating);
+			//System.out.println("antes do if temp: " + temp.maxPeixes + " " + temp.dist + " " + temp.rating);
 
 			//verifica os 3 caso pertendidos, ter o maximo de peixes, ter o minimo de distancia e ter um rating menor, a ultima verificacao e apenas para entrar na primeira vez
 			if (temp.maxPeixes <= s.maxPeixes && temp.dist >= s.dist && temp.rating >= s.rating || i == (barcos.length - 1))
 			{
 				temp.copy(s);
-				System.out.println("temp atualizado: " + temp.maxPeixes + " " + temp.dist + " " + temp.rating);
+				//System.out.println("temp atualizado: " + temp.maxPeixes + " " + temp.dist + " " + temp.rating);
 			}
 		}
 		
@@ -151,108 +131,57 @@ public class CodFishing
 			return temp;
 		}
 
-		//verifica se o a associacao ja se encontra calcula (isto e, com o melhor caso)
-		//caso nao estaja entra no if, caso esteja entra no else para copiar os valores do melhor caso
-		/*if (matriz_sol[pos_b][pos_p] == null || pos_b > 0 && barcos[pos_b].atributo == barcos[pos_b - 1].atributo)
-		{*/
+		if (matriz_sol[pos_b][pos_p] == null)
+		{
+			//se houve mais barcos que peixes por decidir e se a distancia do barco pos_b em relacao ao sitio de peixes for maior que a distancia do barco pos_b - 1 em relacao ao mesmo sitio de peixes
+			//entao vai entrar neste if decrementando o barco
+			if (pos_b > pos_p && Math.abs(barcos[pos_b].x - peixes[pos_p].x) + Math.abs(barcos[pos_b].y - peixes[pos_p].y) > Math.abs(barcos[pos_b - 1].x - peixes[pos_p].x) + Math.abs(barcos[pos_b - 1].y - peixes[pos_p].y))
+				pos_b--;
+
+			//verifica se o a associacao ja se encontra calcula (isto e, com o melhor caso)
+			//caso nao estaja entra no if, caso esteja entra no else para copiar os valores do melhor caso
+
 			//calcula qual o melhor caso do lado esquerdo da arvore
 			Solution s1 = new Solution();
-			System.out.println("1ª pesquisa: entrou com o barco " + pos_b + " e vai para o barco " + (pos_b - 1));
+			//System.out.println("1ª pesquisa: entrou com o barco " + pos_b + " e vai para o barco " + (pos_b - 1));
 			s1.copy(pesquisa(matriz_sol, barcos, peixes, pos_b - 1, pos_p - 1));
-			System.out.println("terminou a 1ª pesquisa do barco " + pos_b + " com o S1: " + s1.maxPeixes + " " + s1.dist + " " + s1.rating);
+			//System.out.println("terminou a 1ª pesquisa do barco " + pos_b + " com o S1: " + s1.maxPeixes + " " + s1.dist + " " + s1.rating);
 
 			//incrementa o numero max de peixes
 			//incrementa a distancia percorrida usando a distancia de manhattan
 			//incrementa o atributo dos barcos
 			s1.inc(barcos[pos_b], peixes[pos_p]);
 
-			System.out.println("apos incremento do barco " + pos_b + " o S1: " + s1.maxPeixes + " " + s1.dist + " " + s1.rating);
+			//System.out.println("apos incremento do barco " + pos_b + " o S1: " + s1.maxPeixes + " " + s1.dist + " " + s1.rating);
 
 			//criar uma instancia para saber qual o melhor caso do lado direito da arvore
 			Solution s2 = new Solution();
-			System.out.println("2ª pesquisa: entrou com o barco " + pos_b + " e vai para o barco " + (pos_b - 2));
+			//System.out.println("2ª pesquisa: entrou com o barco " + pos_b + " e vai para o barco " + (pos_b - 2));
 			s2.copy(pesquisa(matriz_sol, barcos, peixes, pos_b - 2, pos_p - 1));
-			System.out.println("terminou a 2ª pesquisa do barco " + pos_b + " com o S2: " + s2.maxPeixes + " " + s2.dist + " " + s2.rating);
+			//System.out.println("terminou a 2ª pesquisa do barco " + pos_b + " com o S2: " + s2.maxPeixes + " " + s2.dist + " " + s2.rating);
 
 			//incrementa o numero max de peixes
 			//incrementa a distancia percorrida
 			//incrementa o atributo dos barcos
 			s2.inc(barcos[pos_b], peixes[pos_p]);
 
-			System.out.println("apos incremento do barco " + pos_b + " o S2: " + s2.maxPeixes + " " + s2.dist + " " + s2.rating);
+			//System.out.println("apos incremento do barco " + pos_b + " o S2: " + s2.maxPeixes + " " + s2.dist + " " + s2.rating);
 
-			System.out.println("\nantes\nS1: " + s1.maxPeixes + " " + s1.dist + " " + s1.rating);
-			System.out.println("S2: " + s2.maxPeixes + " " + s2.dist + " " + s2.rating);
+			//System.out.println("\nantes\nS1: " + s1.maxPeixes + " " + s1.dist + " " + s1.rating);
+			//System.out.println("S2: " + s2.maxPeixes + " " + s2.dist + " " + s2.rating);
 
 			//verifica se o lado esquerdo tem mais peixes que o lado direito, ou
 			// se o lado esquerdo tem uma distancia menor que o lado direito, ou
 			// se o lado esquerdo tem um rating menor que o lado direito
 			//tem de ser por esta ordem pois o maxPeixes pervalece sobre a dist e o dist pervalece sobre o rating
-			//caso cumpra uma comparacao entra no if, caso contrario entra no else
+			//caso cumpra uma comparacao entra no if
 			if(s1.maxPeixes > s2.maxPeixes || s1.dist < s2.dist || s1.rating < s2.rating)
-			{
-				if(matriz_sol[pos_b][pos_p] == null || matriz_sol[pos_b][pos_p].maxPeixes > s1.maxPeixes || matriz_sol[pos_b][pos_p].dist < s1.dist || matriz_sol[pos_b][pos_p].rating < s1.rating)
-				{
-					matriz_sol[pos_b][pos_p] = s1;
-				}
-			}
+				matriz_sol[pos_b][pos_p] = s1;
 
+			//caso contrario entra no else
 			else
-			{
-				if(matriz_sol[pos_b][pos_p] == null || matriz_sol[pos_b][pos_p].maxPeixes > s2.maxPeixes || matriz_sol[pos_b][pos_p].dist < s2.dist || matriz_sol[pos_b][pos_p].rating < s2.rating)
-				{
-					matriz_sol[pos_b][pos_p] = s2;
-				}
-			}	
-
-				
-
-			/*for (int i = 0; i < barcos.length; i++)
-				System.out.println(barcos[i].x);*/
-
-			if (pos_b > 1 && barcos[pos_b - 1].atributo == barcos[pos_b - 2].atributo)
-			{
-				System.out.println("dentro da pesquisa");
-				for (int i = 0; i < barcos.length; i++)
-					System.out.println(barcos[i].x);
-
-				System.out.println("dentro do pesquisa barco " + (pos_b - 1) + " com rating " + barcos[pos_b - 1].atributo);
-				System.out.println("dentro do pesquisa barco " + (pos_b - 2) + " com rating " + barcos[pos_b - 2].atributo);
-				Item aux_pesquisa = barcos[pos_b - 1];
-				barcos[pos_b - 1] = barcos[pos_b - 2];
-				barcos[pos_b - 2] = aux_pesquisa;
-
-				Solution s3 = new Solution();
-				System.out.println("1ª pesquisa: entrou com o barco " + pos_b + " e vai para o barco " + (pos_b - 1));
-				s3.copy(pesquisa(matriz_sol, barcos, peixes, pos_b - 1, pos_p - 1));
-				System.out.println("terminou a 1ª pesquisa do barco " + pos_b + " com o S1: " + s3.maxPeixes + " " + s3.dist + " " + s3.rating);
-
-				s3.inc(barcos[pos_b], peixes[pos_p]);
-				System.out.println("apos incremento do barco " + pos_b + " o S1: " + s3.maxPeixes + " " + s3.dist + " " + s3.rating);
-				if (s3.maxPeixes > matriz_sol[pos_b][pos_p].maxPeixes || s3.dist < matriz_sol[pos_b][pos_p].dist || s3.rating < matriz_sol[pos_b][pos_p].rating)
-					matriz_sol[pos_b][pos_p] = s3;
-
-				Solution s4 = new Solution();
-
-				System.out.println("2ª pesquisa: entrou com o barco " + pos_b + " e vai para o barco " + (pos_b - 2));
-				s4.copy(pesquisa(matriz_sol, barcos, peixes, pos_b - 2, pos_p - 1));
-				System.out.println("terminou a 2ª pesquisa do barco " + pos_b + " com o S2: " + s4.maxPeixes + " " + s4.dist + " " + s4.rating);
-				s4.inc(barcos[pos_b], peixes[pos_p]);
-
-				System.out.println("apos incremento do barco " + pos_b + " o S2: " + s4.maxPeixes + " " + s4.dist + " " + s4.rating);
-
-				System.out.println("\nantes\nS3: " + s3.maxPeixes + " " + s3.dist + " " + s3.rating);
-				System.out.println("S4: " + s4.maxPeixes + " " + s4.dist + " " + s4.rating);
-
-				if (s4.maxPeixes > matriz_sol[pos_b][pos_p].maxPeixes || s4.dist < matriz_sol[pos_b][pos_p].dist || s4.rating < matriz_sol[pos_b][pos_p].rating)
-					matriz_sol[pos_b][pos_p] = s4;
-
-				aux_pesquisa = barcos[pos_b - 1];
-				barcos[pos_b - 1] = barcos[pos_b - 2];
-				barcos[pos_b - 2] = aux_pesquisa;
-			}
-		//}
-
+				matriz_sol[pos_b][pos_p] = s2;
+		}
 
 		//System.out.println("arr_sol[" + pos_b + "][" + pos_p + "]: " + matriz_sol[pos_b][pos_p].maxPeixes + " " + matriz_sol[pos_b][pos_p].dist + " " + matriz_sol[pos_b][pos_p].rating);
 
